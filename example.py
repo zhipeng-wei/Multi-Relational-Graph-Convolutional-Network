0c49e74310897f4740db23581c08c6543406c0c6
zhipeng-wei <58925637+zhipeng-wei@users.noreply.github.com>
2021-05-06 18:19:27 +0800

    Update example.py

--- example.py
+++ example.py
@@ -7,6 +7,27 @@ flags.DEFINE_string('layer','GraphConvolution_multi_dimension','the type of laye
 
 model_func = GCN_layer_six
 
+# load dataset
+with open('UEC_data.pkl','rb') as ipt:
+    dataset_dict = pkl.load(ipt)
+
+save_information = ['hierarchy', 'ingredient', 'occurrence', 'occurrence1m', 'name_df',
+                    'feature_matrix',
+                    'feature_images','label_images',
+                    'gt_classifier', 'gt_bias',
+                    'gt_classifier_train', 'gt_classifier_test','gcn_train_mask', 'gcn_test_mask',
+                    'evaluation_val_mask','evaluation_test_mask','evaluation_train_mask',
+                    'path_list']
+
+adj1, adj2, adj3_0, adj3_1 = dataset_dict['hierarchy'], dataset_dict['ingredient'], dataset_dict['occurrence'], dataset_dict['occurrence1m']
+features = dataset_dict['feature_matrix']
+shape = dataset_dict['gt_classifier'].shape[0]
+y_train, y_test, train_mask, test_mask = dataset_dict['gt_classifier_train'], dataset_dict['gt_classifier_test'], dataset_dict['gcn_train_mask'], dataset_dict['gcn_test_mask']
+evaluation_trian_mask,evaluation_test_mask,evaluation_val_mask = dataset_dict['evaluation_train_mask'], dataset_dict['evaluation_test_mask'], dataset_dict['evaluation_val_mask'],
+feature_images = dataset_dict['feature_images']
+label_images = dataset_dict['label_images']
+path_list = dataset_dict['path_list'] 
+  
 # You should use your own dataset to construct the placeholder.
 placeholders = {
     'support': [tf.sparse_placeholder(tf.float32) for _ in range(num_supports)],
